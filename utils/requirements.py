eb1f10df508634c2acafc3a0ec0f3a88043655da
Douglas Utz <douglas@utz.net.br>
2024-05-26 23:03:12 -0300

    Change requirements to a separeted .txt file

--- utils/requirements.py
+++ utils/requirements.py
@@ -3,9 +3,6 @@ import sys
 import subprocess
 from utils.messages import *
 
-# Packages
-packages = ['requests', 'beautifulsoup4', 'mysql-connector-python', 'pyodbc']
-
 # Function
 def install_packages():
     '''
@@ -13,8 +10,7 @@ def install_packages():
     '''    
     print(get_message('package_install'))
     try:
-        for package in packages:
-            subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-q', package])
+        subprocess.check_call(['pip', 'install', '-q', '-r', 'requirements.txt'])
     except:
         print(get_message('error_package'))
     else:
